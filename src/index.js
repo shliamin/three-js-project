92fcae5d039783dc2e331cbf73eb4cba671a60e2
Efim Shliamin <shliamin@icloud.com>
2019-08-16 15:21:28 +0300

    creating an animated cube

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
       // scene
       // camera
       // renderer
-      console.log('test');
       // setting up the scene:
       var scene = new THREE.Scene();
       // setting up the camera:
@@ -22,7 +21,7 @@
       // To create a cube we need a BoxGeometry. This is an object that contains all the points (vertices) and fill (faces) of the cube.
       var geometry = new THREE.BoxGeometry(1,1,1);
       // We need a material to color it. 3js comes with several materials, but we will stick to the MeshBasicMaterial for now. All materials take an object of properties which will be applied to them.
-      var material = new There.MeshBasicMaterial({color: 0x00ff00});
+      var material = new THREE.MeshNormalMaterial();
       // We need a Mesh. This is an object that takes a geometry, and applies a material to it, which we can then insert to our scene, and move freely arround.
       var cube = new THREE.Mesh(geometry, material);
       // By default, when we call scene.add(), the thing we add will be added to the coordinates (0,0,0). This would cause both the camera and the cube to be inside each other. To avoid this, we simply move the camera out a bit.
@@ -36,13 +35,11 @@
         // this will create a loop that causes
         // the renderer to draw the scene every
         // time the screen is refreshed
-        // (on a typical screen this means 60 times per second).
-
-        // requestAnimationFrame has a number of advantages.
-        // Perhaps the most important one is that it pauses
-        // when the user navigates to another browser tab,
-        // hence not wasting their precious processing power and battery life.
+        // (on a typical screen this means 60 times)
         requestAnimationFrame( animate );
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
         renderer.render(scene, camera);
       }
       animate();
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./src/three.js",
-  mode: "development",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
-  },
-  devtool: "sourcemap"
-};
-
-
-module.exports = {
   entry: "./src/index.js",
   mode: "development",
   output: {
